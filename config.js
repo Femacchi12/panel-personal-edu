625c512855a4415376e57b0257d85b0ebb3a50a5
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-08-14 12:47:15 -0500

    Publicar navegación, autenticación y sincronización final

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 window.PANEL_CONFIG = {
   googleClientId: "",
+  authProvider: "firebase-google",
+  allowedEmails: [
+    "fernandoemacchi@gmail.com",
+    "eduardo@fibrazo.com"
+  ],
   financeSpreadsheetId: "1ff_dT8kHhiy1THTq1hRHGx2z2VElUQjyq_A4AL_nm4g",
   healthSpreadsheetId: "1I7Z93rrr6J-0-sP9QuBtZrMuu_t1As3lik0_WK8xqMk",
   autoRefreshMinutes: 5,
