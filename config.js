e06835aa3264fc6d37b262e1899f25539e49f282
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-08-14 11:58:34 -0500

    Configura fuentes maestras

---
+++ config.js
@@ -0,0 +1,8 @@
+window.PANEL_CONFIG = {
+  googleClientId: "",
+  financeSpreadsheetId: "1ff_dT8kHhiy1THTq1hRHGx2z2VElUQjyq_A4AL_nm4g",
+  healthSpreadsheetId: "1I7Z93rrr6J-0-sP9QuBtZrMuu_t1As3lik0_WK8xqMk",
+  autoRefreshMinutes: 5,
+  timezone: "America/Bogota",
+  primaryCurrency: "COP"
+};
