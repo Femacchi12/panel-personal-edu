3237cbc86247866468edc8161983c2d8ba82cef6
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-08-26 20:32:44 -0500

    Separate document spreadsheet source

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ window.PANEL_CONFIG = {
     "eduardo@fibrazo.com"
   ],
   financeSpreadsheetId: "1ff_dT8kHhiy1THTq1hRHGx2z2VElUQjyq_A4AL_nm4g",
+  documentsSpreadsheetId: "1P8_zNStHg9v5Xm1loYvT_95TgfRWVUCOY341tyJXDV0",
   healthSpreadsheetId: "1I7Z93rrr6J-0-sP9QuBtZrMuu_t1As3lik0_WK8xqMk",
   autoRefreshMinutes: 5,
   timezone: "America/Bogota",
